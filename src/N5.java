bbe565e11f88ef18d2055f992f4dc4ab523a7113
pavel-ryzhov <88110675+pavel-ryzhov@users.noreply.github.com>
2021-08-02 19:26:58 +0300

    Update N5.java

--- src/N5.java
+++ src/N5.java
@@ -2,9 +2,12 @@ public class N5 {
     public static void main(String[] args) {
         int[] a = {Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])};
         int y = 0;
+        int x = 0;
         for (Integer b : a) {
-            if (b != 0) y++;
+            if (b > 0) y++;
+            else if (b < 0) x++;
         }
-        System.out.println(y);
+        System.out.println("положительные " + y);
+        System.out.println("отрицательные " +  x);
     }
 }
