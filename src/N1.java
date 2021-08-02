6b2621f8c15845da4a0a1648b7efabd5a5977b8c
pavel-ryzhov <88110675+pavel-ryzhov@users.noreply.github.com>
2021-08-02 19:19:22 +0300

    Update N1.java

--- src/N1.java
+++ src/N1.java
@@ -1,13 +1,15 @@
 public class N1 {
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
+        boolean t = a < 0;
         if (a > 0)
             System.out.println("число положительное");
         else if (a < 0)
             System.out.println("число отрицательное");
         else
             System.out.println("число равно 0");
-        switch (args[0].length()){
+
+        switch ((Math.abs(Integer.parseInt(args[0])) + "").length()){
             case 1:
                 System.out.println("число однозначное");
                 break;
@@ -17,8 +19,6 @@ public class N1 {
             case 3:
                 System.out.println("число трёхзначное");
                 break;
-            default:
-                System.out.println("более");
         }
     }
 }
